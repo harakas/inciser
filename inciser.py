a51ad6fe0d3e1ed1550f03f7adb9b483be835102
Indrek Mandre <indrek@mare.ee>
2024-06-23 14:50:49 +0300

    update README

--- inciser.py
+++ inciser.py
@@ -1,17 +1,17 @@
 
-# Python script to use the xTool D1 Pro laser engraver for incising timber
-# Idea is to drill small holes into the wood so it can be impregnated better.
+# Python script to use the xTool D1 Pro laser engraver for incising timber.
+# The idea is to drill small holes into wood so that it can be impregnated better.
 #
 # As xTool engravers lack the dwell command (G4), we need to run the
 # engraver synchronously, meaning we time and wait for it to move and burn
 # and only issue the next command once it has completed the previous.
 #
-# This script is quite hackish but does the job
+# This script is quite hackish but does the job.
 #
 # There are no command line arguments or interactivity, you need to build
-# the program by hand instead
+# the program by hand instead.
 #
-# On an xTool D1 Pro 20W I can burn about three 5-15mm holes per second
+# On an xTool D1 Pro 20W I can burn about three 5-15mm holes per second.
 #
 # If I did this project again I'd probably try to get a different engraver.
 # xTool has their own proprietary firmware and only supports a subset of
